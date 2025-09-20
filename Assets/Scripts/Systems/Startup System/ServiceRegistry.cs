867077b1764091c51c6454a4bd5e227db58a08bb
ZenisVNzz <phuoc0948@gmail.com>
2025-09-21 01:35:52 +0700

    Added ServiceRegistry

--- Assets/Scripts/Systems/Startup System/ServiceRegistry.cs
+++ Assets/Scripts/Systems/Startup System/ServiceRegistry.cs
@@ -1,16 +1,30 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ServiceRegistry : MonoBehaviour
+public class ServiceRegistry : IServiceRegistry
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Dictionary<Type, object> _serviceData = new Dictionary<Type, object>();
+
+    public void Register<TService>(TService service)
     {
-        
+        _serviceData[typeof(TService)] = service;
     }
 
-    // Update is called once per frame
-    void Update()
+    public TService Get<TService>()
     {
-        
+        if (_serviceData.TryGetValue(typeof(TService), out var service)) { return (TService)service; }
+        throw new InvalidOperationException($"Service {typeof(TService).Name} not registered");
+    }
+
+    public bool TryGet<TService>(out TService service)
+    {
+        if (_serviceData.TryGetValue(typeof(TService), out var s))
+        {
+            service = (TService)s;
+            return true;
+        }
+        service = default(TService);
+        return false;
     }
 }
