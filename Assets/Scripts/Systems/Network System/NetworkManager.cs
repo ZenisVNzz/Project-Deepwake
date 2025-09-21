d303e7a9030defe9fff969ac6c0ec721af1257bc
ZenisVNzz <phuoc0948@gmail.com>
2025-09-22 03:54:25 +0700

    Refactor NetworkTask

--- Assets/Scripts/Systems/Network System/NetworkManager.cs
+++ Assets/Scripts/Systems/Network System/NetworkManager.cs
@@ -1,51 +1,20 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using UnityEditor.VersionControl;
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class NetworkManager : IGameService
 {
-    private NetworkServiceList _networkServiceList;
     private IServiceRegistry _serviceRegistry;
+
+    public NetworkManager(IServiceRegistry serviceRegistry)
+    {
+        _serviceRegistry = serviceRegistry;
+    }
 
     public async Task<bool> InitAsync(IServiceRegistry serviceRegistry, CancellationToken ct)
     {
-        AsyncOperationHandle<NetworkServiceList> handle = Addressables.LoadAssetAsync<NetworkServiceList>("NetworkServiceList");
-        _networkServiceList = await handle.Task;
-        _serviceRegistry = new ServiceRegistry();
-
-        foreach (var service in _networkServiceList.NetworkServices)
-        {
-            string ServiceName = service.GetType().Name;
-            Debug.Log($"[Network] Initialize service: {ServiceName}");
-            using (var timeoutCTS = CancellationTokenSource.CreateLinkedTokenSource(ct))
-            {
-                timeoutCTS.CancelAfter(TimeSpan.FromSeconds(10));
-                try
-                {
-                    var o = await service.InitAsync(_serviceRegistry, timeoutCTS.Token);
-                    if (!o)
-                    {
-                        Debug.Log($"[Network] Initialize service failed: {ServiceName}");
-                        return false;
-                    }    
-                }
-                catch (OperationCanceledException)
-                {
-                    Debug.LogWarning($"[Network] Initialize timeout or cancelled: {ServiceName}");
-                    return false;
-                }
-                catch (Exception ex)
-                {
-                    Debug.LogException(ex);
-                    return false;
-                }
-            }    
-        }
         serviceRegistry.Register<NetworkManager>(this);
+        await Task.CompletedTask;
         return true;
     }
 
--- Assets/Scripts/Systems/Startup System/Startup Tasks/InitNetworkTask.cs
+++ Assets/Scripts/Systems/Startup System/Startup Tasks/InitNetworkTask.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 [CreateAssetMenu(fileName = "InitNetworkTask", menuName = "StartupSystem/InitNetworkTask")]
 public class InitNetworkTask : StartupTask
@@ -9,7 +12,41 @@ public class InitNetworkTask : StartupTask
 
     public override async Task<bool> RunTaskAsync(IServiceRegistry serviceRegistry, CancellationToken ct)
     {
-        var Net = new NetworkManager();
+        IServiceRegistry SR = new ServiceRegistry();
+
+        AsyncOperationHandle<NetworkServiceList> handle = Addressables.LoadAssetAsync<NetworkServiceList>("NetworkServiceList");
+        NetworkServiceList networkServiceList = await handle.Task;
+
+        foreach (var service in networkServiceList.NetworkServices)
+        {
+            string ServiceName = service.GetType().Name;
+            Debug.Log($"[Network] Initialize service: {ServiceName}");
+            using (var timeoutCTS = CancellationTokenSource.CreateLinkedTokenSource(ct))
+            {
+                timeoutCTS.CancelAfter(TimeSpan.FromSeconds(10));
+                try
+                {
+                    var o = await service.InitAsync(SR, timeoutCTS.Token);
+                    if (!o)
+                    {
+                        Debug.Log($"[Network] Initialize service failed: {ServiceName}");
+                        return false;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Debug.LogWarning($"[Network] Initialize timeout or cancelled: {ServiceName}");
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                    return false;
+                }
+            }
+        }
+
+        var Net = new NetworkManager(SR);
         await Net.InitAsync(serviceRegistry, ct);
 
         return true;
