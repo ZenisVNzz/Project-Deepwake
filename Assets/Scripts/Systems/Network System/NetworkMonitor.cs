aba5fcc26f1b1e6f18bf834f76f235c8382d5f09
ZenisVNzz <phuoc0948@gmail.com>
2025-09-21 19:40:09 +0700

    Fixed NetworkMonitor not timeout when offline

--- Assets/Scripts/Systems/Network System/NetworkMonitor.cs
+++ Assets/Scripts/Systems/Network System/NetworkMonitor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -11,9 +12,12 @@ public class NetworkMonitor : NetworkService
     private readonly string _pingUrl = "https://www.google.com";
     private readonly int _interval = 5;
 
+    private CancellationTokenSource _cts;
+
     public override async Task<bool> InitAsync(IServiceRegistry serviceRegistry, CancellationToken ct)
     {
-        _isOnline = await CheckConnectionAsync();
+        _cts = new CancellationTokenSource();
+        _isOnline = await CheckConnectionAsync(_cts.Token);
         Debug.Log($"[NetworkMonitor] Client status: {(_isOnline ? "Online" : "Offline")}");
         serviceRegistry.Register<NetworkMonitor>(this);
         _ = MonitorLoop(ct);
@@ -24,18 +28,33 @@ public class NetworkMonitor : NetworkService
     {
         while (!ct.IsCancellationRequested)
         {
-            _isOnline = await CheckConnectionAsync();
+            _isOnline = await CheckConnectionAsync(_cts.Token);
             await Task.Delay(_interval * 1000, ct);
         }
     }
 
-    private async Task<bool> CheckConnectionAsync()
+    private async Task<bool> CheckConnectionAsync(CancellationToken ct)
     {
         using (var req = UnityWebRequest.Get(_pingUrl))
         {
-            var op = req.SendWebRequest();
-            while (!op.isDone) await Task.Yield();
-            return req.result == UnityWebRequest.Result.Success;
-        }    
+            using (var timeoutCTS = CancellationTokenSource.CreateLinkedTokenSource(ct))
+            {
+                timeoutCTS.CancelAfter(TimeSpan.FromSeconds(5));
+                var op = req.SendWebRequest();
+
+                while (!op.isDone)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        req.Abort();
+                        return false;
+                    }
+                    await Task.Yield();
+                }
+
+                return req.result == UnityWebRequest.Result.Success;
+            }    
+            
+        }
     }
 }
