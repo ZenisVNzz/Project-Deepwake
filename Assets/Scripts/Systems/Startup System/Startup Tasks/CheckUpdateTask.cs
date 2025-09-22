57c4a6b32ee87c391bc35ed3adffbb3ccb501156
ZenisVNzz <phuoc0948@gmail.com>
2025-09-22 22:07:09 +0700

    Added check game version task

---
+++ Assets/Scripts/Systems/Startup System/Startup Tasks/CheckUpdateTask.cs
@@ -0,0 +1,56 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CheckUpdateTask", menuName = "StartupSystem/CheckUpdateTask")]
+public class CheckUpdateTask : StartupTask
+{
+    public override bool HasTimeout { get { return true; } }
+
+    public override async Task<bool> RunTaskAsync(IServiceRegistry serviceRegistry, CancellationToken ct)
+    {
+        Debug.Log($"[CheckUpdateTask] Check for Update.");
+        bool checkResult = await CheckGameVersionAsync();
+        if (checkResult)
+        {
+            return true;
+        }    
+        return false;
+    }    
+
+    private async Task<bool> CheckGameVersionAsync()
+    {
+        var task = new TaskCompletionSource<bool>();
+
+        Debug.Log($"[CheckUpdateTask] Send GetTitleData request.");
+        PlayFabClientAPI.GetTitleData(new GetTitleDataRequest(), result =>
+        {
+            if (result.Data != null && result.Data.ContainsKey("Game_CurrentVersion"))
+            {
+                string serverVersion = result.Data["Game_CurrentVersion"];
+                Debug.Log($"[PlayFab] Latest game version on server: {serverVersion}");
+                string localVersion = Application.version;
+
+                if (localVersion != serverVersion)
+                {
+                    Debug.LogWarning($"[CheckUpdateTask] Game is outdated.");
+                    task.SetResult(false);
+                }
+                else
+                {
+                    Debug.LogWarning($"[CheckUpdateTask] Game is up to date.");
+                    task.SetResult(true);
+                }
+            }
+        },
+        error =>
+        {
+            Debug.LogError("[PlayFab] Failed to get TitleData: " + error.GenerateErrorReport());
+            task.SetResult(false);
+        });
+
+        return await task.Task;
+    }    
+}
