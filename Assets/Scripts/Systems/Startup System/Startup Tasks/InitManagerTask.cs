28fd4f445eb78f4020d7451b16e7ad66378fc041
ZenisVNzz <phuoc0948@gmail.com>
2025-09-25 02:05:54 +0700

    Added Initialize Managers task

---
+++ Assets/Scripts/Systems/Startup System/Startup Tasks/InitManagerTask.cs
@@ -0,0 +1,70 @@
+﻿using Mirror.BouncyCastle.Crypto.Generators;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InitManagerTask", menuName = "StartupSystem/InitManagerTask")]
+public class InitManagerTask : StartupTask
+{
+    [SerializeField] private string _managersRoot;
+    [SerializeField] private List<MonoScript> _script;
+    public override bool HasTimeout { get { return true; } }
+
+    public override async Task<bool> RunTaskAsync(IServiceRegistry serviceRegistry, CancellationToken ct)
+    {
+        Transform managerRoot = GameObject.Find(_managersRoot).transform;
+
+        if (managerRoot == null)
+        {
+            Debug.LogError("[InitManagerTask] ManagersRoot is not assign yet");
+            return false;
+        }
+
+        if (_script == null)
+        {
+            Debug.LogError("[InitManagerTask] Script is not assign yet");
+            return false;
+        }
+
+        foreach (var script in _script)
+        {
+            var type = script.GetClass();
+
+            var component = managerRoot.GetComponent(type);
+            if (component == null)
+            {
+                component = managerRoot.gameObject.AddComponent(type);
+            }
+
+            IManager manager = (IManager)component;
+            await manager.InitAsync();
+
+            serviceRegistry.Register(component);
+        }    
+
+        return true;
+    }
+
+    private void OnValidate()
+    {
+        List<MonoScript> scriptsToRemove = new List<MonoScript>();
+        foreach (var script in _script)
+        {
+            var type = script.GetClass();
+
+            if (!typeof(IManager).IsAssignableFrom(type))
+            {
+                Debug.LogError($"[InitManagerTask] {script.name} is not correct type. The type of script must implement IManager");
+                scriptsToRemove.Add(script);
+            }
+        }
+        foreach (var config in scriptsToRemove)
+        {
+            _script.Remove(config);
+        }
+    }
+}
