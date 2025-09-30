eb4020aa0ea9e6cdc0d2172fec30abd4da513a9b
ZenisVNzz <phuoc0948@gmail.com>
2025-09-30 19:09:34 +0700

    Added object shadow cast

--- Assets/Scripts/Object/ObjectShadowCast.cs
+++ Assets/Scripts/Object/ObjectShadowCast.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ObjectShadowCast : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject _shadowObject;
+
+    private void Awake()
     {
-        
+        CastShadow();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CastShadow()
     {
-        
+        if (_shadowObject != null) return;
+
+        SpriteRenderer originalRenderer = GetComponent<SpriteRenderer>();
+
+        if (originalRenderer == null) return;
+        _shadowObject = new GameObject($"{name}_Shadow");
+        _shadowObject.transform.SetParent(transform);
+
+        _shadowObject.transform.localScale = new Vector3(1f, 1.5f, 1f);
+        _shadowObject.transform.localPosition = new Vector3(0f, 0.04f, 0f);
+        _shadowObject.transform.localRotation = Quaternion.Euler(45f, 0f, -100f);
+
+        SpriteRenderer shadowRenderer = _shadowObject.AddComponent<SpriteRenderer>();
+        shadowRenderer.sprite = originalRenderer.sprite;
+        shadowRenderer.color = new Color(0f, 0f, 0f, 0.6f);
+        shadowRenderer.sortingLayerID = originalRenderer.sortingLayerID;
+        shadowRenderer.sortingOrder = originalRenderer.sortingOrder - 1;
     }
 }
