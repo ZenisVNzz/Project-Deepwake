bec205c873fbe7f30fbdf28bd7b5392f7ca3ad75
ZenisVNzz <phuoc0948@gmail.com>
2025-09-30 20:18:37 +0700

    Added time cycle

--- Assets/Scripts/Object/ObjectShadowCast.cs
+++ Assets/Scripts/Object/ObjectShadowCast.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class ObjectShadowCast : MonoBehaviour
 {
     private GameObject _shadowObject;
@@ -29,5 +28,52 @@ public class ObjectShadowCast : MonoBehaviour
         shadowRenderer.color = new Color(0f, 0f, 0f, 0.6f);
         shadowRenderer.sortingLayerID = originalRenderer.sortingLayerID;
         shadowRenderer.sortingOrder = originalRenderer.sortingOrder - 1;
+
+        GameTimeController gameTimeController = FindFirstObjectByType<GameTimeController>();
+        if (gameTimeController != null && !gameTimeController.ShadowCasters.Contains(this))
+        {
+            gameTimeController.ShadowCasters.Add(this);
+        }
+    }
+
+    public void transparency(float value)
+    {
+        _shadowObject.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, value);
+    }
+
+    public void UpdateShadowTransparency(int hour, int minute = 0)
+    {
+        float timeOfDay = hour + minute / 60f;
+        float alpha;
+
+        if (timeOfDay >= 6f && timeOfDay <= 18f)
+        {
+            float t = (timeOfDay - 6f) / 12f;
+            alpha = Mathf.Lerp(0.3f, 0.6f, Mathf.Cos((t - 0.5f) * Mathf.PI) * 0.5f + 0.5f);
+        }
+        else if (timeOfDay >= 5f && timeOfDay < 6f)
+        {
+            float t = (timeOfDay - 5f) / 1f;
+            alpha = Mathf.Lerp(0.1f, 0.3f, t);
+        }
+        else if (timeOfDay > 18f && timeOfDay <= 19f)
+        {
+            float t = (timeOfDay - 18f) / 1f;
+            alpha = Mathf.Lerp(0.3f, 0.1f, t);
+        }
+        else
+        {
+            alpha = 0.1f;
+        }
+
+        transparency(alpha);
+    }
+
+    public void RotateByTime(int hour, int minute)
+    {
+        float t = (hour + minute / 60f) / 24f; 
+        float angle = t * 360f; 
+
+        _shadowObject.transform.localRotation = Quaternion.Euler(45f, 0f, angle);
     }
 }
---
+++ Assets/Scripts/Systems/GameTimeSystem/GameTimeController.cs
@@ -0,0 +1,107 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class GameTimeController : MonoBehaviour
+{
+    [Header("Time Settings")]
+    [SerializeField] private int hour = 6;
+    [SerializeField] private int minute = 0;
+    [SerializeField] private float timeSpeed = 60f; 
+
+    [Header("Lights")]
+    [SerializeField] private Light2D _globalLight;
+    [SerializeField] private Light2D _sunLight;
+
+    [Header("Objects With Shadow")]
+    public List<ObjectShadowCast> ShadowCasters = new List<ObjectShadowCast>();
+
+    private float timer;
+
+    void Update()
+    {
+        TimeProcess();
+        ShadowProcess();
+        LightingProcess();
+    }
+
+    private void TimeProcess()
+    {
+        timer += Time.deltaTime * timeSpeed;
+
+        if (timer >= 60f)
+        {
+            minute++;
+            timer = 0f;
+
+            if (minute >= 60)
+            {
+                minute = 0;
+                hour++;
+                if (hour >= 24)
+                    hour = 0;
+            }
+        }
+    }  
+    
+    private void ShadowProcess()
+    {
+        foreach (var shadowCaster in ShadowCasters)
+        {
+            shadowCaster.UpdateShadowTransparency(hour, minute);
+            shadowCaster.RotateByTime(hour, minute);
+        }
+    }
+
+    private void LightingProcess()
+    {
+        float t = (hour + minute / 60f) / 24f;
+
+        float sunAngle = t * 360f;
+        if (_sunLight != null)
+            _sunLight.transform.rotation = Quaternion.Euler(0f, 0f, sunAngle);
+
+        // Ban ngày (6h -> 18h)
+        if (hour >= 6 && hour <= 18)
+        {
+            float daylight = Mathf.InverseLerp(6f, 18f, hour + minute / 60f);
+
+            if (_sunLight != null)
+            {
+                _sunLight.intensity = Mathf.Lerp(0f, 0.5f, Mathf.Sin(daylight * Mathf.PI));
+            }
+
+            if (_globalLight != null)
+            {
+                _globalLight.intensity = 0.7f;
+            }
+        }
+        else if (hour > 18 && hour < 20) // Hoàng hôn (18h -> 20h)
+        {
+            float dusk = Mathf.InverseLerp(18f, 20f, hour + minute / 60f);
+            if (_globalLight != null)
+            {
+                _globalLight.intensity = Mathf.Lerp(0.7f, 0.3f, dusk);
+            }
+        }
+        else if (hour >= 20 || hour < 4) // Ban đêm (20h -> 6h)
+        {
+            if (_sunLight != null)
+                _sunLight.intensity = 0f;
+
+            if (_globalLight != null)
+            {
+                _globalLight.intensity = 0.3f;
+            }
+        }
+        else if (hour >= 4 && hour < 6) // Bình minh (4h -> 6h)
+        {
+            float dawn = Mathf.InverseLerp(4f, 6f, hour + minute / 60f);
+            if (_globalLight != null)
+            {
+                _globalLight.intensity = Mathf.Lerp(0.3f, 0.7f, dawn);
+            }
+        }
+    }
+}
