bcc5d33d1ee010b973aacec3c3cc2c4259becaa0
ZenisVNzz <phuoc0948@gmail.com>
2025-10-09 05:12:37 +0700

    Added A* Pathfinding

---
+++ Assets/Scripts/Character/Enemy/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+    public float nextWaypointDistance = 1f;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating(nameof(UpdatePath), 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null) return;
+        if (currentWaypoint >= path.vectorPath.Count) return;
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * 40f * Time.deltaTime;
+        rb.linearVelocity = force;
+
+        if (Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+            currentWaypoint++;
+    }
+}
