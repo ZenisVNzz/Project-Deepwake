f2eba493cf26a4755211f618ae609dd20569c3e5
ZenisVNzz <phuoc0948@gmail.com>
2025-10-25 10:26:11 +0700

    Added map UI

---
+++ Assets/Scripts/UI/Map/MapNode.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MapNode : MonoBehaviour
+{
+    public MapNodeData nodeData;
+    private Button button;
+    public List<MapNode> linkedNode = new();
+    public bool IsInteractable = true;
+    public UnityAction<MapNode> OnNodeClicked;
+
+    private Image OutlineImage;
+    private Image IconImage;
+
+    public void Init(MapNodeData nodeData)
+    {
+        this.nodeData = nodeData;
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnSelect);
+        OutlineImage = GetComponent<Image>();
+        IconImage = transform.Find("Icon").GetComponent<Image>();
+        IconImage.sprite = nodeData.nodeType.image;
+    }
+
+    public void OnSelect()
+    {
+        if (IsInteractable)
+        {
+            OnNodeClicked?.Invoke(this);
+            nodeData.OnSelect();
+        }    
+    }
+
+    public void SetInteractable(bool value)
+    {
+        IsInteractable = value;
+
+        if (button != null)
+            button.interactable = value;
+
+        if (IconImage != null)
+            IconImage.color = value ? Color.white : Color.gray;
+    }
+
+    public void MarkAsCurrent(bool value)
+    {
+        if (OutlineImage != null)
+            OutlineImage.color = value ? Color.red : (IsInteractable ? Color.white : Color.gray);
+    }
+}
