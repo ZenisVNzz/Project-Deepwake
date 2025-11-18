e6f92ba22c5a07e0e06fb7b2aa80077e91b329d2
ZenisVNzz <phuoc0948@gmail.com>
2025-11-18 20:36:44 +0700

    Revert "Fixed Google Sign In"
    This reverts commit bfb4374e2c32c4c71698a7f42fa5b122e2334257.

--- Assets/Scripts/Systems/Account/GoogleLoginHandler.cs
+++ Assets/Scripts/Systems/Account/GoogleLoginHandler.cs
@@ -1,29 +1,95 @@
-﻿using UnityEngine;
-using i5.Toolkit.Core.OpenIDConnectClient;
-using i5.Toolkit.Core.ServiceCore;
+﻿using UnityEngine; 
+using Google;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+using UnityEngine.UI;
 
-public class GoogleLoginHandler : BaseServiceBootstrapper
+public class GoogleLoginHandler : MonoBehaviour
 {
-    [SerializeField] private ClientDataObject googleClientDataObject;
-    [SerializeField] private ClientDataObject googleClientDataObjectEditor;
+    public string GoogleWebAPI = "1042625468516-40manp0l7o04alqc71m9uqulaq28n7aj.apps.googleusercontent.com";
 
-    protected override void RegisterServices()
+    private GoogleSignInConfiguration configuration;
+
+    Firebase.DependencyStatus dependencyStatus = Firebase.DependencyStatus.UnavailableOther;
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+
+    private void Awake()
     {
-        OpenIDConnectService oidc = new OpenIDConnectService();
-        oidc.OidcProvider = new GoogleOidcProvider();
+        configuration = new GoogleSignInConfiguration
+        {
+            WebClientId = GoogleWebAPI,
+            RequestIdToken = true
+        };
 
-#if UNITY_EDITOR             
-        oidc.OidcProvider.ClientData = googleClientDataObjectEditor.clientData;
-#elif PLATFORM_ANDROID
-        oidc.OidcProvider.ClientData = googleClientDataObject.clientData;
-#else
-        oidc.OidcProvider.ClientData = googleClientDataObjectEditor.clientData;
-#endif
-        ServiceManager.RegisterService<OpenIDConnectService>(oidc);
+        Button loginButton = GetComponent<Button>();
+        loginButton.onClick.AddListener(GoogleSignInClick);
     }
 
-    protected override void UnRegisterServices()
+    private void Start()
     {
-        throw new System.NotImplementedException();
+        InitFireBase();
+    }
+
+    void InitFireBase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+
+    void GoogleSignInClick()
+    {
+        GoogleSignIn.Configuration = configuration;
+        GoogleSignIn.Configuration.UseGameSignIn = false;
+        GoogleSignIn.Configuration.RequestIdToken = true;
+        GoogleSignIn.Configuration.RequestEmail = true;
+
+        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(
+          OnAuthenticationFinished);
+    }
+
+    void OnAuthenticationFinished(Task<GoogleSignInUser> task)
+    {
+        if (task.IsFaulted)
+        {
+            Debug.LogError("Google Sign-In encountered an error: " + task.Exception);
+        }
+        else if (task.IsCanceled)
+        {
+            Debug.LogWarning("Google Sign-In was canceled.");
+        }
+        else
+        {
+            Firebase.Auth.Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential(task.Result.IdToken, null);
+
+            auth.SignInWithCredentialAsync(credential).ContinueWithOnMainThread(authTask =>
+            {
+                if (authTask.IsFaulted)
+                {
+                    Debug.LogError("Firebase authentication failed: " + authTask.Exception);
+                }
+                else if (authTask.IsCanceled)
+                {
+                    Debug.LogWarning("Firebase authentication was canceled.");
+                }
+                else
+                {
+                    user = auth.CurrentUser;
+                    Debug.LogFormat("User signed in successfully: {0} ({1})", user.DisplayName, user.UserId);
+                }
+            });
+
+            StartupProcessor.Instance.GetService<PlayFabServiceManager>().GetService<PlayFabClient>().GoogleLoginAsync(task.Result.IdToken).ContinueWith(playFabTask =>
+            {
+                if (playFabTask.IsFaulted || playFabTask.IsCanceled)
+                {
+                    Debug.LogError("PlayFab Google login failed.");
+                }
+                else if (playFabTask.Result)
+                {
+                    Debug.Log("PlayFab Google login succeeded.");
+                }
+            });
+        }
     }
 }
